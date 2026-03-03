7efe24a0937d5373062ec95c975f54c6e4533e8e
arwegasira <ac.rwegasira@gmail.com>
2026-03-03 20:22:38 +0200

    libraries & packages set up

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import './App.css'
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
